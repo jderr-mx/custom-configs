d92b38ea98dd5a2b896853808c1b9c3f0727bbe6
John Derr <john.derr@maximos.io>
2019-04-13 16:26:47 -0400

    Added npm_lazy alias

--- zsh-custom/zsh-custom.sh
+++ zsh-custom/zsh-custom.sh
@@ -3,3 +3,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 alias cc-exam="ember exam --split=3 --weighted --parallel"
 export PATH="$(brew --prefix php@7.1)/bin:$PATH"
+alias npm-lazy="npm_lazy --config /Users/john/npm_lazy.config.js"
