f1da815551dc76ac3a6329820781ad4cfd8581ff
John Derr <john@allovue.com>
2019-11-27 10:54:33 -0500

    Added airline config, custom startify, aliases

--- zsh-custom/aliases.sh
+++ zsh-custom/aliases.sh
@@ -1,4 +1,12 @@
 alias npm-lazy="npm_lazy --config /Users/john/npm_lazy.config.js"
-alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql@9.6.plist"
-alias pg_stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql@9.6.plist"
+alias pg_start="launchctl load /usr/local/opt/postgresql@11/homebrew.mxcl.postgresql.plist"
+alias pg_stop="launchctl unload /usr/local/opt/postgresql@11/homebrew.mxcl.postgresql.plist"
 alias git-dev="git checkout development"
+alias urldecode='python -c "import sys, urllib as ul; \
+    print ul.unquote_plus(sys.argv[1])"'
+
+alias urlencode='python -c "import sys, urllib as ul; \
+    print ul.quote_plus(sys.argv[1])"'
+alias migrate-dev="rake db:migrate RAILS_ENV=development"
+alias rails-serve="USE_MAILCATCHER=1 rails s"
+alias run-spec="bundle exec rspec --t \~elasticsearch"
--- zsh-custom/zsh-custom.sh
+++ zsh-custom/zsh-custom.sh
@@ -1,6 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-alias cc-exam="ember exam --split=3 --weighted --parallel"
+alias ember-exam="ember exam --split=2 --parallel"
 export PATH="$(brew --prefix postgresql@9.6)/bin:$PATH"
 alias npm-lazy="npm_lazy --config /Users/john/npm_lazy.config.js"
+[[ $TMUX = "" ]] && export TERM="screen-256color"
